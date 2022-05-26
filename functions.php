323df31498a7b7825695b152839314eefafae864
luciasupova <luciasupovaa@gmail.com>
2022-05-26 16:11:55 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,8 +7,10 @@ add_action('wp_enqueue_scripts',
         get_template_directory_uri().'/style.css');
     }
 
-    /*STYLE ORDER*/  
+    /*STYLESHEET ORDER*/  
     add_action( 'elementor/frontend/after_enqueue_styles', function() {
         wp_enqueue_style( 'elementor-post-1025-css', get_stylesheet_uri(), NULL, filemtime( get_stylesheet_directory() . '/style.css' ) );
     ;
     } );
+
+
