53caac004f154708b5e63019addcf9d0d58e2daa
luciasupova <luciasupovaa@gmail.com>
2022-05-30 02:30:10 +0200

    updated changes

--- functions.php
+++ functions.php
@@ -12,5 +12,3 @@ add_action('wp_enqueue_scripts',
         wp_enqueue_style( 'elementor-post-1025-css', get_stylesheet_uri(), NULL, filemtime( get_stylesheet_directory() . '/style.css' ) );
     ;
     } );
-
-
